e3f56c782f10b1f6b2044884d16a0b902147d07d
akbokha <a.k.bokharouss@student.tue.nl>
2017-02-03 13:10:28 +0100

    updated load_data.R -> Removed last row with only NA values + sorted on Total Actuals et cetera

--- load_data.R
+++ load_data.R
@@ -1,5 +1,14 @@
 library(readxl)
 ncdata <- read_excel("C:/_Storage/asm/170120 Non Conformance Data 2013.xlsx")
+#remove last row with only NA values
+ncdata <- head(ncdata, -1)
 
-#same data, but without the rows (nc's) that have Total actuals == 0 euro
-ncdata_posactuals <- ncdata[ncdata$`Total Actuals`>0,]
+#same data as ncdata, but without the rows (nc's) that have Total actuals == 0 euro
+ncdata_posactuals <- ncdata[ncdata$`Total Actuals`!=0.0,]
+
+#same data, but sorted (increasing on Total Actuals)
+ncdata_posactuals_inc <- ncdata_posactuals[order(ncdata_posactuals$`Total Actuals`, decreasing = FALSE),]
+
+
+#same data as ncdata, but only the rows (nc's) that have Total actuals ==0
+ncdata_zeroactuals <- ncdata[ncdata$`Total Actuals`==0.0,]
