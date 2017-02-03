084a088e805bf68077e1f6c7f8545b560aac4700
akbokha <a.k.bokharouss@student.tue.nl>
2017-02-03 13:36:26 +0100

    Plot to show the distribution of the total actuals per product line

---
+++ total_act_vis.R
@@ -0,0 +1,11 @@
+library(plotly)
+
+# This shows the distribution of the total actuals per product line
+
+totalActualsProductLines <- plot_ly(ncdata_posactuals, x = ~ncdata_posactuals$`Total Actuals`, color=~ncdata_posactuals$`Product Line`, type = "box") %>%
+  layout(
+    title = "Total Actuals Per Product-Line",
+    xaxis = list(         
+      title = "Total Actuals", 
+      showgrid = T)
+  )
