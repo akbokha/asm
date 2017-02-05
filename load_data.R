0aa23110cdd121476db4bbe33ff233a6a73b5a8b
Creemers <s.p.g.creemers@student.tue.nl>
2017-02-05 21:47:19 +0100

    Added import of plot libraries

--- load_data.R
+++ load_data.R
@@ -1,4 +1,6 @@
 library(readxl)
+library(ggplot2)
+library(plotly)
 ncdata <- read_excel("C:/_Storage/asm/170120 Non Conformance Data 2013.xlsx")
 #remove last row with only NA values
 ncdata <- head(ncdata, -1)
@@ -12,3 +14,4 @@ ncdata_posactuals_inc <- ncdata_posactuals[order(ncdata_posactuals$`Total Actual
 
 #same data as ncdata, but only the rows (nc's) that have Total actuals ==0
 ncdata_zeroactuals <- ncdata[ncdata$`Total Actuals`==0.0,]
+
