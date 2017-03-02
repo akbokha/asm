52ae74cd05adae76623f7ce4bbb7558125d4f22f
akbokha <a.k.bokharouss@student.tue.nl>
2017-03-02 15:52:52 +0100

    splitting of customer impact attribute

---
+++ validation/split_cust_impact.R
@@ -0,0 +1,43 @@
+#split the attribute 'Customer Impact Description' into three new attributes
+#these attributes will contain the strings "yes"and "no"
+
+ncdata$Safety <- NA
+ncdata$WaferScrap <- NA
+ncdata$Downtime <- NA
+
+# Safety Attribute
+for (i in 1:5258) {
+  if (grepl("No Safety", ncdata[i,]$`Customer Impact Descr`, fixed = TRUE)) {
+    ncdata[i,]$Safety <- "No"
+  }
+  else {
+    if (!(grepl("Not Assigned", ncdata[i,]$`Customer Impact Descr`, fixed = TRUE))) {
+      ncdata[i,]$Safety <- "Yes"
+    }
+  }
+}
+
+
+# WaferScrap Attribute
+for (i in 1:5258) {
+  if (grepl("^No wafer Scrap$", ncdata[i,]$`Customer Impact Descr`, fixed = TRUE)) {
+    ncdata[i,]$WaferScrap <- "No"
+  }
+  else {
+    if (!(grepl("Not Assigned", ncdata[i,]$`Customer Impact Descr`, fixed = TRUE))) {
+      ncdata[i,]$WaferScrap <- "Yes"
+    }
+  }
+}
+
+# Downtime Attribute
+for (i in 1:5258) {
+  if (grepl("No Downtime", ncdata[i,]$`Customer Impact Descr`, fixed = TRUE)) {
+    ncdata[i,]$Downtime <- "No"
+  }
+  else {
+    if (!(grepl("Not Assigned", ncdata[i,]$`Customer Impact Descr`, fixed = TRUE))) {
+      ncdata[i,]$Downtime <- "Yes"
+    }
+  }
+}
