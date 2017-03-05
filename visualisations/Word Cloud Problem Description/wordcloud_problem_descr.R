359f51d8efafeb958e4b07a1e17567f6062cec64
akbokha <a.k.bokharouss@student.tue.nl>
2017-03-05 19:59:08 +0100

    Fixed the wordcloud (plotting, removed words et cetera)

--- visualisations/Word Cloud Problem Description/wordcloud_problem_descr.R
+++ visualisations/Word Cloud Problem Description/wordcloud_problem_descr.R
@@ -11,6 +11,9 @@ ncdata_corp <- Corpus(VectorSource(ncdata$`Problem descr`))
 # convert corpus to plain text document
 ncdata_corp <- tm_map(ncdata_corp, PlainTextDocument)
 
+#Make all the words lower case
+ncdata_corp <- tm_map(ncdata_corp, tolower)
+
 #remove punctuation and stopwords
 ncdata_corp <- tm_map(ncdata_corp, removePunctuation)
 ncdata_corp <- tm_map(ncdata_corp, removeWords, stopwords('english'))
@@ -18,6 +21,21 @@ ncdata_corp <- tm_map(ncdata_corp, removeWords, stopwords('english'))
 #stemming of words
 #ncdata_corp < tm_map(ncdata_corp, stemDocument)
 
-ncdata_corp <- Corpus(VectorSource(ncdata_corp))
+#remove words that are common in the problem description box, but are not of value
+#examples: 2013, onecomm, call, cause et cetera
+
+common_words <- c("2013", "number", "changed", "call", "onecomm", "2014", "issue",
+                  "call", "cause", "number", "changed", "during", "jan", "feb", "mar", "apr",
+                  "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec", "rootcause", 
+                  "problem", "open", "can", "due", "one", "line", "used", "need", "following",
+                  "needs", "will", "karl", "found")
+remove <- data.frame(common_words)
+ncdata_corp <- tm_map(ncdata_corp, removeWords, remove$common_words)
+
 #plot wodcloud
-wordcloud(ncdata_corp, min.freq=30, max.words=50, random.order=TRUE, ordered.colors = TRUE, use.r.layout = TRUE)
+ncdata_corp <- Corpus(VectorSource(ncdata_corp))
+#plot wordcloud -> colors
+pal <- brewer.pal(8,"Dark2")
+#actual plotting of the wordcloud
+wordcloud(ncdata_corp, min.freq=30, max.words=80, use.r.layout = TRUE,
+          colors=pal, scale = c(6, 0.8, 1.8))
