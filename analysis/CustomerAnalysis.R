d290b5b79a3b76f76334e1c67e4b821404970210
Wilhelm <j.wilhelm@student.tue.nl>
2017-03-26 16:59:57 +0200

    Earchive analyses

---
+++ analysis/CustomerAnalysis.R
@@ -0,0 +1,51 @@
+NrOfCat(AllCustomers_MachineData$Customer.x)
+#41 bedrijven, nc's verwijderen
+AllCustomers_MachineData <- alldata2[!is.na(alldata2$Customer.x),]
+length(AllCustomers_MachineData)
+
+#bar chart genereren
+Standard_Bar_NC(AllCustomers_MachineData$Customer.x,"Customer", "Number of NC's")
+
+#Vergelijken met earchive
+NrOfCat(earchive_correct$Customer)
+#84 categoriën
+earchive_correct <- earchive[!is.na(earchive$Customer),]
+Standard_Bar_NC(earchive_correct$Customer)
+#Blijkt dat earchive andere namen gebruikt, 5 grootste manueel uitzoeken:
+
+NumberOfCustomerNCs <- aggregate(data.frame(count = AllCustomers_MachineData$Customer.x), list(count_by = AllCustomers_MachineData$Customer.x), length)
+NumberOfCustomerNCs <- as.data.frame(NumberOfCustomerNCs)
+setDT(NumberOfCustomerNCs, keep.rownames = TRUE)
+colnames(NumberOfCustomerNCs) <- c("ID", "Customer", "Number of NC's")
+
+OrderedCustomerNCs <- NumberOfCustomerNCs[order(NumberOfCustomerNCs$`Number of NC's`),]
+OrderedCustomerNCs
+
+#Intel; 548, Global Foundries; 141, Infineon; 132, Rober Bosch GmbH; 125
+#Look these up in earchive: IC; Global_Foundries, Infineon, BOSCH
+NumberOfCustomerE <- aggregate(data.frame(count = earchive_correct$Customer), list(count_by = earchive_correct$Customer), length)
+NumberOfCustomerE <- as.data.frame(NumberOfCustomerE)
+setDT(NumberOfCustomerE, keep.rownames = TRUE)
+colnames(NumberOfCustomerE) <- c("ID", "Customer", "Number of NC's")
+
+OrderedCustomerE <- NumberOfCustomerE[order(NumberOfCustomerE$`Number of NC's`),]
+OrderedCustomerE
+#IC; 159, Infineon; 148, BOSCH; 26, Global_Foundries; 12
+#Count average number of NC's per product for these
+NrNCsIntel <- 548 / 159
+NrNCsInfineon <- 132 / 148
+NrNcsBosch <- 125 / 26
+NrNcsGlobalFoundries <- 141 / 12
+
+#Zet ze in een data frame
+Averages = c(NrNCsIntel, NrNCsInfineon, NrNcsBosch, NrNcsGlobalFoundries)
+Companies = c("Intel", "Infineon", "Bosch", "Global Foundries")
+AverageNCPerProduct <- data.frame(Companies, Averages)
+AverageNCPerProduct <- AverageNCPerProduct[order(AverageNCPerProduct$Averages),]
+plot_ly(AverageNCPerProduct, x=~AverageNCPerProduct$Companies, y=~AverageNCPerProduct$Averages, type = 'bar', mode = 'markers') %>%
+  layout(
+    title = "Average Number of NC's per product"
+  )
+
+Standard_Bar_NC(AllCustomers_MachineData$Customer.y,"Customer", "Number of NC's")
+Standard_Bar_NC(AllCustomers_MachineData$Customer.x,"Customer", "Number of NC's")
--- analysis/Earchive_NCdata_Analysis.R
+++ analysis/Earchive_NCdata_Analysis.R
@@ -13,4 +13,18 @@ View(alldata)
 #geeft 1900 rows -> best bruikbare dataset, hoeveel unieke waardes?
 length(unique(alldata$PN))
 
-#245 unieke waardes. 600 NA's
+#245 unieke waardes. 632 NA's
+sum(is.na(alldata$PN))
+sum(is.na(ncdata$PN))
+#NA's in alldata komen als NA niet gespecificeerd is in ncdata
+#alle rows met NA in PN verwijderen:
+alldata2 <- alldata[!is.na(alldata$PN),]
+length(unique(alldata2$PN))
+
+#244 unieke waardes.
+length(alldata2$PN)
+#1286 rows
+
+length(unique(alldata2$Customer.x))
+#41 bedrijven hierin
+sum(is.na(alldata2$Customer.x))
