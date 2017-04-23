2f3b3d5ef2248cf0f1ff15d8b1e2e777900595a4
akbokha <a.k.bokharouss@student.tue.nl>
2017-04-23 15:00:14 +0200

    Fixed merging issue

--- analysis/Manufacturing & Warranty Analysis/phases_relations.R
+++ analysis/Manufacturing & Warranty Analysis/phases_relations.R
@@ -1,1 +1,16 @@
-# new subdirectory
+# make sure to load ncdata again (wihout modifications that could affect this analysis) in your global environment
+
+# removing all the records (approx. 600) that are non-customer related
+# these records have NA values for the Equipment descr attribute and the NC Equipment Phase attribute
+
+ncdata_phases <- ncdata[!is.na(ncdata$`Equipment descr`),]
+
+# removed 632 records (/5258)
+# check if also all the records that are "Non-customer related" got removed
+
+ncdata_phases <- ncdata_phases[ncdata_phases$`NC Equipment Phase` != "Non Customer related",]
+#all the records @line 4 got removed
+# count unique machines in the dataset
+
+unique_count <- unique(ncdata_phases$`Equipment descr`)
+unique_count <- length(unique_count)
