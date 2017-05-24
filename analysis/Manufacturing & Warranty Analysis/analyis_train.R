3b8b2de8f27069d2b7415c754d40fe7d2c32f545
akbokha <a.k.bokharouss@student.tue.nl>
2017-05-24 23:09:23 +0200

    modifications to classification tree

--- analysis/Manufacturing & Warranty Analysis/analyis_train.R
+++ analysis/Manufacturing & Warranty Analysis/analyis_train.R
@@ -3,6 +3,7 @@
 # note that te for loops in phases_adjustdata and phases_relations take long to complete
 # so try to execute them once and then keep them in your workspace
 library(rpart)
+library(rpart.plot)
 
 # records_to_analyse is the dataframe in which all the records are found that have a NC in the manufacturing/I&Q phase
 # and later in the warranty/contract phase again
@@ -15,16 +16,26 @@ train_set <- man_contract_phase
 train_set$`Cause Group` = as.factor(train_set$`Cause Group`)
 train_set$`Defect Group` = as.factor(train_set$`Defect Group`)
 train_set$`Product Line` = as.factor(train_set$`Product Line`)
+train_set$`Material type` = as.factor(train_set$`Material type`)
+train_set$`Order type` = as.factor(train_set$`Order type`)
+train_set$`Consumable` = as.factor(train_set$`Consumable`)
+train_set$`Product Line` = as.factor(train_set$`Product Line`)
+train_set$`Equipment Status` = as.factor(train_set$`Equipment Status`)
+train_set$`NC Disposition` = as.factor(train_set$`NC Disposition`)
+train_set$`Material type` = as.factor(train_set$`Material type`)
 
-fit <- rpart(NC_WarrantyContract ~ `Cause Group` + `Defect Group` + `Material type` + `Order type` + `Consumable`
-             + `Product Line` + `Equipment Status` + `NC Disposition`,
+fit <- rpart(NC_WarrantyContract ~ `Defect Group` 
+             + `Product Line` + `Consumable` + 
+               `Equipment Status` + `NC Disposition`,
              data=train_set,
-             method="class", control=rpart.control(minsplit=2, minbucket=1, cp=0.001))
+             method="class", control=rpart.control(minsplit = 2, minbucket = 11, cp = 0.001))
 
 test_model$`Cause Group` = as.factor(test_model$`Cause Group`)
 test_model$`Defect Group` = as.factor(test_model$`Defect Group`)
+test_model$`Consumable` = as.factor(test_model$`Consumable`)
 
 test_prediction <- predict(fit, test_model, type = "class")
 
 misClasificError <- mean(test_prediction != test_man_contract_phase$NC_WarrantyContract)
 print(paste('Accuracy',1-misClasificError))
+rpart.plot(fit)
