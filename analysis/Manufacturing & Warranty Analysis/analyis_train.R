8027aad89be92d9a0ea03c6d78444b09be8d4ce6
akbokha <a.k.bokharouss@student.tue.nl>
2017-05-29 11:41:30 +0200

    final modifications to classification tree and random forest

--- analysis/Manufacturing & Warranty Analysis/analyis_train.R
+++ analysis/Manufacturing & Warranty Analysis/analyis_train.R
@@ -13,16 +13,10 @@ library(rpart.plot)
 # warranty/contract phases
 train_set <- man_contract_phase
 
-train_set$`Cause Group` = as.factor(train_set$`Cause Group`)
 train_set$`Defect Group` = as.factor(train_set$`Defect Group`)
 train_set$`Product Line` = as.factor(train_set$`Product Line`)
-train_set$`Material type` = as.factor(train_set$`Material type`)
-train_set$`Order type` = as.factor(train_set$`Order type`)
 train_set$`Consumable` = as.factor(train_set$`Consumable`)
-train_set$`Product Line` = as.factor(train_set$`Product Line`)
 train_set$`Equipment Status` = as.factor(train_set$`Equipment Status`)
-train_set$`NC Disposition` = as.factor(train_set$`NC Disposition`)
-train_set$`Material type` = as.factor(train_set$`Material type`)
 
 fit <- rpart(NC_WarrantyContract ~ `Defect Group` 
              + `Product Line` + `Consumable` + 
@@ -30,12 +24,13 @@ fit <- rpart(NC_WarrantyContract ~ `Defect Group`
              data=train_set,
              method="class", control=rpart.control(minsplit = 2, minbucket = 11, cp = 0.005))
 
-test_model$`Cause Group` = as.factor(test_model$`Cause Group`)
 test_model$`Defect Group` = as.factor(test_model$`Defect Group`)
+test_model$`Product Line` = as.factor(test_model$`Product Line`)
 test_model$`Consumable` = as.factor(test_model$`Consumable`)
+test_model$`Equipment Status` = as.factor(test_model$`Equipment Status`)
 
 test_prediction <- predict(fit, test_model, type = "class")
 
 misClasificError <- mean(test_prediction != test_man_contract_phase$NC_WarrantyContract)
 print(paste('Accuracy',1-misClasificError))
-rpart.plot(fit)
+# rpart.plot(fit)
--- analysis/Manufacturing & Warranty Analysis/randomFtraining.R
+++ analysis/Manufacturing & Warranty Analysis/randomFtraining.R
@@ -2,16 +2,10 @@
 library(randomForest)
 
 train_set <- man_contract_phase
-train_set$`Cause Group` = as.factor(train_set$`Cause Group`)
 train_set$`Defect Group` = as.factor(train_set$`Defect Group`)
 train_set$`Product Line` = as.factor(train_set$`Product Line`)
-train_set$`Material type` = as.factor(train_set$`Material type`)
-train_set$`Order type` = as.factor(train_set$`Order type`)
 train_set$`Consumable` = as.factor(train_set$`Consumable`)
-train_set$`Product Line` = as.factor(train_set$`Product Line`)
 train_set$`Equipment Status` = as.factor(train_set$`Equipment Status`)
-train_set$`NC Disposition` = as.factor(train_set$`NC Disposition`)
-train_set$`Material type` = as.factor(train_set$`Material type`)
 
 fit2 <- randomForest(NC_WarrantyContract ~ train_set$`Defect Group`
              + train_set$`Product Line` + train_set$`Consumable` + 
@@ -19,9 +13,10 @@ fit2 <- randomForest(NC_WarrantyContract ~ train_set$`Defect Group`
              data=train_set,
              importance=TRUE, ntree = 2000, na.action=na.exclude)
 
-test_model$`Cause Group` = as.factor(test_model$`Cause Group`)
 test_model$`Defect Group` = as.factor(test_model$`Defect Group`)
+test_model$`Product Line` = as.factor(test_model$`Product Line`)
 test_model$`Consumable` = as.factor(test_model$`Consumable`)
+test_model$`Equipment Status` = as.factor(test_model$`Equipment Status`)
 
 test_prediction <- predict(fit2, test_model)
 
